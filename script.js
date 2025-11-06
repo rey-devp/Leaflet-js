d3a6256167a36626b1c603a3916dcecb15703f29
rrq-dev <raihanhermawan85@gmail.com>
2025-11-07 00:56:06 +0700

    Fix map view and marker coordinates in script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var map = L.map("map").setView([53.463956428380335, -2.291187623593197], 13);
+var map = L.map("map").setView([51.505, -0.09], 13);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker([53.463956428380335, -2.291187623593197])
+L.marker([51.5, -0.09])
   .addTo(map)
-  .bindPopup("Ini Marker")
+  .bindPopup("A pretty CSS popup.<br> Easily customizable.")
   .openPopup();
