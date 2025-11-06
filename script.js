e4b8550092a4574877abb55fcaaea8194daca96e
rrq-dev <raihanhermawan85@gmail.com>
2025-11-07 00:54:09 +0700

    Update map initialization and marker details in script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// inisialisasi peta
-var map = L.map("map").setView([-6.8743902175547085, 107.57576416732226], 13);
+var map = L.map("map").setView([53.463956428380335, -2.291187623593197], 13);
 
-// Tambahkan tile layer dari OpenStreetMap
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-// Tambahkan marker
-L.marker([-6.8743902175547085, 107.57576416732226])
+L.marker([53.463956428380335, -2.291187623593197])
   .addTo(map)
-  .bindPopup("Custom Marker")
+  .bindPopup("Ini Marker")
   .openPopup();
